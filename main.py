bf21427fd2d28ae1f89baad5cae860311180bc24
Yifeng Qin <>
2021-05-31 09:57:05 +0000

    Check if the Wikipeida Links are valid and keeps checking for one if it's invalid

--- main.py
+++ main.py
@@ -34,15 +34,6 @@ def choose_word():
 
   return word, link
 
-# def print_message(message, start_word, start_link, end_word, end_link):
-
-#     start = '**Start Word: **' + start_word
-#     end = '**End Word: **' + end_word
-#     message.channel.send(start)
-#     message.channel.send(start_link)
-#     message.channel.send('====================================================================================')
-#     message.channel.send(end)
-#     message.channel.send(end_link)
 
 @client.event
 async def on_ready():
@@ -67,40 +58,3 @@ async def on_message(message):
     await message.channel.send(end_link)
 
 client.run(my_secret)
-
- # if db["responding"]:
-  #   options = starter_encouragements
-  #   if "encouragements" in db.keys():
-  #     options = options + db["encouragements"]
-
-  #   if any(word in msg for word in sad_words):
-  #     await message.channel.send(random.choice(options))
-
-  # if msg.startswith("$new"):
-  #   encouraging_message = msg.split("$new ",1)[1]
-  #   update_encouragements(encouraging_message)
-  #   await message.channel.send("New encouraging message added.")
-
-  # if msg.startswith("$del"):
-  #   encouragements = []
-  #   if "encouragements" in db.keys():
-  #     index = int(msg.split("$del",1)[1])
-  #     delete_encouragment(index)
-  #     encouragements = db["encouragements"]
-  #   await message.channel.send(encouragements)
-
-  # if msg.startswith("$list"):
-  #   encouragements = []
-  #   if "encouragements" in db.keys():
-  #     encouragements = db["encouragements"]
-  #   await message.channel.send(encouragements)
-
-  # if msg.startswith("$responding"):
-  #   value = msg.split("$responding ",1)[1]
-
-  #   if value.lower() == "true":
-  #     db["responding"] = True
-  #     await message.channel.send("Responding is on.")
-  #   else:
-  #     db["responding"] = False
-  #     await message.channel.send("Responding is off.")
